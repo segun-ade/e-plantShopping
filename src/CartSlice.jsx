391fd63ea7c488071d247dde606ffd27c10f0604
Adeyemi Segun <seguncongrat@gmail.com>
2026-01-28 11:36:56 -0500

    edit cartslice

--- src/CartSlice.jsx
+++ src/CartSlice.jsx
@@ -22,7 +22,8 @@ export const CartSlice = createSlice({
 
     },
     removeItem: (state, action) => {
-        state.items = state.items.filter(item => item.name !== action.payload);
+        state.items = state.items.filter(item => item.name !== action.payload.name);
+        
     },
     updateQuantity: (state, action) => {
         const { name, quantity } = action.payload; // Destructure the product name and new quantity from the action payload
